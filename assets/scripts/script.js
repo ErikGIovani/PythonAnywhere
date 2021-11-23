1e165655cb5eeb7c3f527a0a3151842af51a0e70
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2021-11-22 21:18:46 -0600

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+let $video = document.getElementById('video');
+
+function transform(){
+    $video.innerHTML = '<iframe src="https://www.youtube.com/embed/NH2PhXYvrWs?autoplay=1" allowfullscreen></iframe>';
+}
